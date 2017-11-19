dc45563c36260aba8096215129d9738de8b979fe
Dylan Hickson <dylan.hickson@gmail.com>
2017-11-18 23:06:29 -0500

    Add notes to main.js and ensure image references are case sensitive

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,14 +1,17 @@
-$(document).ready(function() {
-  $('a[href^="#"]').on('click',function (e) {
-    e.preventDefault();
+$(document).ready(function() {                      // Begin every .js with this //
+  $('a[href^="#"]').on('click',function (e) {       // Select all 'a' elements that start with (^=) "#", on click run some function for event (click) e { //
+    e.preventDefault();                             // Stop normal behavour for link //
 
-    var target = this.hash;
-    var $target = $(target);
+    var target = this.hash;                         // Target hash ("#") that has been clicked (this element) as variable //
+    var $target = $(target);                        // Set variable as jquery object //
 
-    $('html, body').animate({
-        'scrollTop': $target.offset().top
-    }, 1000, 'swing', function () {
-        window.location.hash = target;
+    $('html, body').animate({                       // Select html and body elements and run jquery animate function //
+        'scrollTop': $target.offset().top           // Parameter of animate, pass in $target and set offset to top of div //
+    }, 1000, 'swing', function () {                 // Set timing to 1000ms, choose 'swing' animation start callback function //
+        window.location.hash = target;              // (Optional), when clicked, sets hash in window to whatever was clicked ? //
+
     });
   });
 });
+
+// https://www.youtube.com/watch?v=_EGO-d9H184  <-- Tutorial where code is from //
